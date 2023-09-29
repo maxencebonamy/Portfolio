a2e19f2d3a28579dd9d0c715b205ddfcaa7fe567
maxencebonamy <maxencebonamy@gmail.com>
2023-09-29 19:03:05 +0200

    Responsive Navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -27,13 +27,16 @@ const Navbar = ({ toggleTheme, lang, toggleLang, width }: NavbarProps) => {
         return 1
     }
 
-    const navListWidth = document.querySelector(".nav-list")?.clientWidth ?? width
+    const navListWidth =
+        document.querySelector(".nav-list")?.clientWidth ?? width
     const openSpanStyle = {
-        transform: `translateX(-${width >= BREAKPOINTS.lg ? navListWidth - 32 : width - 48*3}px)`
+        transform: `translateX(-${
+            width >= BREAKPOINTS.lg ? navListWidth - 32 : width - 48 * 3
+        }px)`,
     }
     const ulStyle = width >= BREAKPOINTS.lg ? {} : { width: "100%" }
     const mdLinkStyle = {
-        opacity: isOpen ? "1" : "0"
+        opacity: isOpen ? "1" : "0",
     }
 
     return (
@@ -44,12 +47,39 @@ const Navbar = ({ toggleTheme, lang, toggleLang, width }: NavbarProps) => {
                         <ThemeToggle toggleTheme={toggleTheme} />
                         <LangToggle lang={lang} toggleLang={toggleLang} />
                     </>
-                ) : <></>}
+                ) : (
+                    <></>
+                )}
             </span>
+            {
+                width >= BREAKPOINTS.lg ? <></> : (
+                    <div className={cn(isOpen ? "-translate-y-0" : "-translate-y-full", "fixed w-screen h-screen left-0 top-0 bg-light dark:bg-dark transition-all duration-500 z-10 mobile-nav flex flex-col items-center justify-center gap-8")}>
+                        {
+                            langTexts[lang].navbar.map((text, index) => (
+                                <a
+                                    href={`#${langTexts.en.navbar[
+                                        index
+                                    ].toLowerCase()}`}
+                                    key={index}
+                                    onClick={() => setIsOpen(false)}
+                                    className="cursor-pointer text-dark dark:text-light transition-all duration-500 flex flex-col items-center justify-start"
+                                >
+                                    <h2 className="text-6xl font-extrabold [color:transparent] stroke-dark dark:stroke-light italic">{index + 1}</h2>
+                                    <p className="text-transition uppercase text-xl">{text}</p>
+                                </a>
+                            ))
+                        }
+                    </div>
+                )
+            }
             <ul
                 style={ulStyle}
-                onMouseEnter={width >= BREAKPOINTS.lg ? () => setIsOpen(true) : () => {}}
-                onMouseLeave={width >= BREAKPOINTS.lg ? () => setIsOpen(false) : () => {}}
+                onMouseEnter={
+                    width >= BREAKPOINTS.lg ? () => setIsOpen(true) : () => {}
+                }
+                onMouseLeave={
+                    width >= BREAKPOINTS.lg ? () => setIsOpen(false) : () => {}
+                }
                 className="nav-list h-8 min-w-[3rem] flex items-center justify-center gap-8 px-20"
             >
                 {width >= BREAKPOINTS.lg ? (
@@ -58,7 +88,9 @@ const Navbar = ({ toggleTheme, lang, toggleLang, width }: NavbarProps) => {
                             onMouseEnter={() => setSelectedItem(text)}
                             onMouseLeave={() => setSelectedItem(null)}
                             style={{ opacity: getLinkOpacity(text) }}
-                            href={`#${langTexts.en.navbar[index].toLowerCase()}`}
+                            href={`#${langTexts.en.navbar[
+                                index
+                            ].toLowerCase()}`}
                             key={index}
                             className="cursor-pointer text-dark dark:text-light transition-all duration-500 text-xl uppercase"
                         >
@@ -66,7 +98,10 @@ const Navbar = ({ toggleTheme, lang, toggleLang, width }: NavbarProps) => {
                         </a>
                     ))
                 ) : (
-                    <div style={mdLinkStyle} className="flex flex-row justify-center items-center gap-8 transition-all duration-500">
+                    <div
+                        style={mdLinkStyle}
+                        className={cn(width >= BREAKPOINTS.lg ? "" : "z-20", "flex flex-row justify-center items-center gap-8 transition-all duration-500")}
+                    >
                         <ThemeToggle toggleTheme={toggleTheme} />
                         <LangToggle lang={lang} toggleLang={toggleLang} />
                     </div>
@@ -74,27 +109,29 @@ const Navbar = ({ toggleTheme, lang, toggleLang, width }: NavbarProps) => {
 
                 <span
                     className={cn(
-                        "absolute transition-all duration-500 h-[0.15rem] w-12 bg-dark dark:bg-light rounded-full top-[2.925rem] right-12",
-                        isOpen ? "-rotate-45 translate-y-4" : "")}
+                        "absolute transition-all duration-500 h-[0.15rem] w-12 bg-dark dark:bg-light rounded-full top-[2.925rem] right-12 z-20",
+                        isOpen ? "-rotate-45 translate-y-4" : ""
+                    )}
                 />
                 <span
                     style={isOpen ? openSpanStyle : {}}
-                    className="absolute transition-all duration-500 h-[0.15rem] w-12 bg-dark dark:bg-light rounded-full top-[3.925rem] right-12"
+                    className="absolute transition-all duration-500 h-[0.15rem] w-12 bg-dark dark:bg-light rounded-full top-[3.925rem] right-12 z-20"
                 />
                 <span
                     className={cn(
-                        "absolute transition-all duration-500 h-[0.15rem] w-12 bg-dark dark:bg-light rounded-full top-[4.925rem] right-12",
-                        isOpen ? "rotate-45 -translate-y-4" : "")}
+                        "absolute transition-all duration-500 h-[0.15rem] w-12 bg-dark dark:bg-light rounded-full top-[4.925rem] right-12 z-20",
+                        isOpen ? "rotate-45 -translate-y-4" : ""
+                    )}
                 />
 
-                {
-                    width < BREAKPOINTS.lg ?
+                {width < BREAKPOINTS.lg ? (
                     <span
                         onClick={() => setIsOpen(!isOpen)}
-                        className="absolute h-12 w-12 top-10 right-12"
-                    /> :
+                        className="absolute h-12 w-12 top-10 right-12 z-20"
+                    />
+                ) : (
                     <></>
-                }
+                )}
             </ul>
         </nav>
     )
