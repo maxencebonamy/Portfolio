36209f235fe6acdb82fb7f730893e697a4e6d5ab
Maxence Bonamy <maxencebonamy@gmail.com>
2023-11-10 12:29:43 +0100

    Fix font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,12 @@ import { Poppins } from "next/font/google"
 import "./globals.css"
 import type { PropsWithChildren, ReactElement } from "react"
 
-const font = Poppins({ subsets: ["latin"] })
+const font = Poppins({
+	subsets: ["latin"],
+	display: "swap",
+	variable: "--font-poppins",
+	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"]
+})
 
 export const metadata: Metadata = {
 	title: "Maxence Bonamy | Portfolio",
